7c08a3155054fb3901a2b9f8b1b052157e651366
barchitect0 <tbng84@gmail.com>
2024-10-28 09:49:44 +0700

    fix

--- pinocchio.py
+++ pinocchio.py
@@ -17,7 +17,7 @@ class PinocchioZKP:
     proof = sha256((str(self.secret) + str(random_factor)).encode()).hexdigest()
     return proof, random_factor
 
-  # def verify_proof(self, proof, random_factor):
+  def verify_proof(self, proof, random_factor):
   #   # Simulate proof verification
   #   expected_proof = sha256((str(self.secret) + str(random_factor)).encode()).hexdigest()
   #   return proof == expected_proof
