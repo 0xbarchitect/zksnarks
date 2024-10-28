68e62bb5c68428ef5903497e063a990dcfe158a7
barchitect0 <tbng84@gmail.com>
2024-10-28 09:48:16 +0700

    fix

--- pinocchio.py
+++ pinocchio.py
@@ -3,7 +3,7 @@ from hashlib import sha256
 
 # pinocchio.py
 class PinocchioZKP:
-  # def __init__(self, secret):
+  def __init__(self, secret):
   #   self.secret = secret
   #   self.public_key = self.generate_public_key()
 
