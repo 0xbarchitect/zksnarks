e81fcbab250eda81fb6b2bcb68e2883cd055a3b1
barchitect0 <tbng84@gmail.com>
2024-10-29 16:39:55 +0700

    fix

--- groth16.py
+++ groth16.py
@@ -2,7 +2,7 @@ from py_ecc import bn128
 from py_ecc.bn128 import FQ, FQ2, FQ12, G1, G2, add, multiply, neg, pairing
 
 # Example parameters for Groth16
-# class Groth16Params:
+class Groth16Params:
 #   def __init__(self):
 #     self.g1 = G1
 #     self.g2 = G2
