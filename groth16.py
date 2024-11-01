75fdd2bc451a1df6717dcbdfd7748e0e4b606424
barchitect0 <tbng84@gmail.com>
2024-11-01 09:40:02 +0700

    minor fix

--- groth16.py
+++ groth16.py
@@ -39,5 +39,5 @@ witness = 7
 proof = generate_proof(params, keys, witness)
 public_input = 7
 
-# is_valid = verify_proof(params, keys, proof, public_input)
+is_valid = verify_proof(params, keys, proof, public_input)
 # print(f"Proof is valid: {is_valid}")
